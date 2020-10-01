c1721581ef4723631d786b489c7353e5db902757
Alexey <alexey49035@gmail.com>
2020-10-01 08:14:13 +0300

    initial commit

---
+++ Chat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+    }
+}
